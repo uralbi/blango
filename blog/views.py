ee5bc79668ec99392caf5509cd4df5432d32770e
Ural Kozhokmatov <ural46@gmail.com>
2023-03-01 21:24:38 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+# from django.views.decorators.cache import cache_page
+# from django.views.decorators.vary import vary_on_cookie
 from blog.models import Post
 from blog.forms import CommentForm
 import logging
