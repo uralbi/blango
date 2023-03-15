426c0b378737a48d641b8974b5c3be60b71092f3
Ural Kozhokmatov <ural46@gmail.com>
2023-03-15 20:18:23 +0000

    finish drf views

--- blango/urls.py
+++ blango/urls.py
@@ -13,7 +13,7 @@ import blango_auth.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("blog.api_urls")),
+    path("api/v1/", include("blog.api.urls")),
     path('', blog.views.index),
     path("post/<slug>/", blog.views.post_detail, 
         name="blog-post-detail"),
