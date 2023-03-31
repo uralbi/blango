e57be14a35615b0f8fdff4b6ae3ef1b72c18fc1b
Ural Kozhokmatov <ural46@gmail.com>
2023-03-31 17:57:01 +0000

    js classes

--- blog/static/blog.js
+++ blog/static/blog.js
@@ -1,1 +1,49 @@
-alert('Hello, world!')
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
+
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
+  }
+}
+
+const g = new Greeter('Patchy')  // Put your name here if you like
+g.greet()
+
+class DelayedGreeter extends Greeter {
+  delay = 2000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
+}
+
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
+
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
