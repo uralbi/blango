5122fe317b73f4b6e630e4b0c5e8e1b573a02ac9
Ural Kozhokmatov <ural46@gmail.com>
2023-03-31 18:15:19 +0000

    js promises

--- blog/static/blog.js
+++ blog/static/blog.js
@@ -47,3 +47,30 @@ dg2.greet()
 
 const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
 dg1.greet()
+
+
+function resolvedCallback(data) {
+  console.log('Resolved with data ' +  data)
+}
+
+function rejectedCallback(message) {
+  console.log('Rejected with message ' + message)
+}
+
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers")
+    } else {
+      const sum = a + b
+      resolve(sum)
+    }
+  }
+
+  return new Promise(doAdd)
+}
+
+const p = lazyAdd(3, 4)
+p.then(resolvedCallback, rejectedCallback)
+
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
